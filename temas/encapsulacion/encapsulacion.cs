c8de1d7c30ddbf00e5bbbff0b377ab44766cd42f
María Woodruff Vázquez <96314197+mariawv080201@users.noreply.github.com>
2023-02-28 18:59:48 +0100

    Update encapsulacion.cs

--- temas/encapsulacion/encapsulacion.cs
+++ temas/encapsulacion/encapsulacion.cs
@@ -1,16 +1,44 @@
 
-class Circulo
+using System;
+
+public class circulo
 {
-    private double radio;
+        private double radio;
 
-    public Circulo(double radio)
-    {
-        this.radio = radio;
-    }
+        public circulo(double radio)
+        {
+            this.radio = radio;
+        }
 
-    public double Radio
+        public double Area()
+        {
+            return Math.PI * radio * radio;
+        }
+
+        public double Perimetro()
+        {
+            return 2 * Math.PI * radio;
+        }
+
+        public double Radio
+        {
+            get { return radio; }
+            set { radio = value; }
+        }
+}
+
+
+namespace Application
+{
+    public class Program
     {
-        get { return radio; }
-        set { radio = value; }
-    }
+        public static void Main(string[] args)
+        {
+            double radio = 5.0;
+            circulo c = new circulo(radio);
+
+            Console.WriteLine("El área del círculo es: {0}", c.Area());
+            Console.WriteLine("El perímetro del círculo es: {0}", c.Perimetro());
+        }
+    } 
 }
