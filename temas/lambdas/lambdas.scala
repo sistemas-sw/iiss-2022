83a18248f54b0f9421fc2f5a8905df638f691035
María Woodruff Vázquez <96314197+mariawv080201@users.noreply.github.com>
2023-04-11 19:14:19 +0200

    Delete lambdas.scala

--- temas/lambdas/lambdas.scala
+++
@@ -1,5 +0,0 @@
-object Main extends App {
-    // Ejemplo de función anónima en Scala 3
-    val lambda = (x: Int, y: Int) => x + y
-    println("Resultado de función anónima en Scala 3: " + lambda(3, 5))
-}
